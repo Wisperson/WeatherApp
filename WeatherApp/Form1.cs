2d7125294642b6c4a6a4ab6dce26d5b1820050dd
Wisperson <wisperson7@gmail.com>
2023-12-11 20:58:51 +0600

    Добавьте файлы проекта.

---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace WeatherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string API = "aa8ef15afb238a27d5d434ae0d7b8de8";
+            string City = "Almaty";
+            string URL = $"http://api.openweathermap.org/data/2.5/weather?q={City}&appid={API}";
+            WebRequest request = WebRequest.Create(URL);
+        }
+    }
+}
---
+++ WeatherApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static async Task Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            string API = "aa8ef15afb238a27d5d434ae0d7b8de8";
+            string City = "Almaty";
+            string URL = $"http://api.openweathermap.org/data/2.5/weather?q={City}&appid={API}";
+            //WebRequest request = WebRequest.Create(URL);
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string response = await client.GetStringAsync(URL);
+
+                    Console.WriteLine(response);
+                }
+                catch(HttpRequestException e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+        }
+    }
+}
