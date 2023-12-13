7de350a056f2edb347d2c40c8e43ec7d373fa672
Wisperson <wisperson7@gmail.com>
2023-12-13 08:03:19 +0600

    добавил обработку отсутствия интернета/связи с OpenWeatherMap.com

--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -24,38 +24,50 @@ namespace WeatherApp
         {
             string City = "Almaty";
             string URL = $"http://api.openweathermap.org/data/2.5/weather?q={City}&appid={Secrets.API}&units=metric&lang=ru";
-            WebRequest request = WebRequest.Create(URL);
-
-            request.Method = "POST";
-            request.ContentType = "application/x-www.urlencoded";
-            WebResponse response = await request.GetResponseAsync();
-
-            string answer = string.Empty;
-
-            using(Stream s = response.GetResponseStream())
+            try
             {
-                using(StreamReader reader = new StreamReader(s))
+                WebRequest request = WebRequest.Create(URL);
+
+                request.Method = "POST";
+                request.ContentType = "application/x-www.urlencoded";
+                WebResponse response = await request.GetResponseAsync();
+
+                string answer = string.Empty;
+
+                using (Stream s = response.GetResponseStream())
                 {
-                    answer = await reader.ReadToEndAsync();
+                    using (StreamReader reader = new StreamReader(s))
+                    {
+                        answer = await reader.ReadToEndAsync();
+                    }
                 }
+                response.Close();
+
+                richTextBox1.Text = answer;
+
+                OpenWeather.OpenWeather OW = JsonConvert.DeserializeObject<OpenWeather.OpenWeather>(answer);
+
+                panel1.BackgroundImage = OW.Weather[0].Icon;
+
+                label1.Text = OW.Weather[0].Main;
+                label2.Text = OW.Weather[0].Description;
+                label3.Text = "Средняя температура(°C): " + OW.Main.Temp.ToString("0.##");
+                label4.Text = "Влажность: " + OW.Main.Humidity.ToString() + "%";
+                label5.Text = "Давление(мм р.с.): " + ((int)OW.Main.Pressure).ToString();
+
+                label6.Text = "Скорость(м/с): " + OW.Wind.Speed.ToString();
+                OW.Wind.SetDegString();
+                label7.Text = "Направление: " + OW.Wind.DegString;
             }
-            response.Close();
-
-            richTextBox1.Text = answer;
-
-            OpenWeather.OpenWeather OW = JsonConvert.DeserializeObject<OpenWeather.OpenWeather>(answer);
-
-            panel1.BackgroundImage = OW.Weather[0].Icon;
-
-            label1.Text = OW.Weather[0].Main;
-            label2.Text = OW.Weather[0].Description;
-            label3.Text = "Средняя температура(°C): " + OW.Main.Temp.ToString("0.##");
-            label4.Text = "Влажность: " + OW.Main.Humidity.ToString() + "%";
-            label5.Text = "Давление(мм р.с.): " + ((int)OW.Main.Pressure).ToString();
-
-            label6.Text = "Скорость(м/с): " + OW.Wind.Speed.ToString();
-            OW.Wind.SetDegString();
-            label7.Text = "Направление: " + OW.Wind.DegString;
+            catch (System.Net.WebException ex)
+            {
+                richTextBox1.Text = "Проблема с интернетом";
+            }
+            catch
+            {
+                richTextBox1.Text = "Возникла проблема";
+            }
+           
 
             /*Button helloButton = new Button();
             helloButton.BackColor = Color.LightGray;
